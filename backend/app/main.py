a83042fc57bd8aad584b9667ea5d013eb17ea5d5
shovan <shovanpaul48@gmail.com>
2026-02-25 20:22:21 +0530

    fixing

--- backend/app/main.py
+++ backend/app/main.py
@@ -71,8 +71,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type"],
+    # allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    # allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # ── Routers ───────────────────────────────────────────────────────────────────
